565b8b15c58a43387f947db0da164efe3f95c7ad
junhee <limjunhee4575@gmail.com>
2024-06-22 19:48:12 +0900

    feat.버튼컴포넌트 생성, 컬러 컨벤션

--- book-forest/src/pages/BookRecommendation.jsx
+++ book-forest/src/pages/BookRecommendation.jsx
@@ -1,5 +1,11 @@
+import Button from "../components/Button";
 const BookRecommendation = () => {
-  return <div>BookRecommendation</div>;
+  return (
+    <div>
+      <div>BookRecommendation</div>
+      <Button text={"작성"} type={"color-3"}></Button>
+    </div>
+  );
 };
 
 export default BookRecommendation;
