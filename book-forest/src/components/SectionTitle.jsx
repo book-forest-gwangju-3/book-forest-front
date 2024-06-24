bd85aaa37ffc2be64b213680ad3141bdd04efbfc
junhee <limjunhee4575@gmail.com>
2024-06-25 02:06:35 +0900

    feat.커밋 잔디밭 UI완성

--- book-forest/src/components/SectionTitle.jsx
+++ book-forest/src/components/SectionTitle.jsx
@@ -1,5 +1,5 @@
 const SectionTitle = ({ text, type }) => {
-  const titleClass = `${type} text-2xl font-semibold my-6`;
+  const titleClass = `${type} text-2xl my-6`;
   return <h2 className={titleClass}>{text}</h2>;
 };
 export default SectionTitle;
