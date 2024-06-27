7d8c6a25856bd512ad1db680890f7be716a6d191
junhee <limjunhee4575@gmail.com>
2024-06-27 20:21:47 +0900

    fix.store 수정

--- book-forest/src/components/LoginForm.jsx
+++ book-forest/src/components/LoginForm.jsx
@@ -1,63 +1,80 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { setUser } from "./../features/user/userSlice";
+import { setUser, setUserInfo } from "./../features/user/userSlice";
 import axios from "axios";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = (e) => {
     e.preventDefault();
 
-    const formData = new FormData()
+    const formData = new FormData();
 
-    formData.append("password", password)
-    formData.append("username", username)
+    formData.append("password", password);
+    formData.append("username", username);
 
-    axios.post('http://localhost:8080/login', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    }).then(response => {
-      const token = response.headers.authorization.replace("Bearer ", "")
-      dispatch(setUser({ token }));
-      
-      console.log('로그인 성공')
-      navigate("/");
-    }).catch(error => {
-      console.log('로그인에 실패했습니다. 다시 시도해주세요', error);
-    });
+    axios
+      .post("http://localhost:8080/login", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        const token = response.headers.authorization.replace("Bearer ", "");
+        dispatch(setUser({ token }));
+
+        console.log("로그인 성공");
+        return axios.get("http://localhost:8080/user/my-info", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      })
+      .then((userInfoResponse) => {
+        dispatch(setUserInfo(userInfoResponse.data));
+        console.log("사용자 정보 저장 성공");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(
+          "로그인 또는 사용자 정보 요청에 실패했습니다. 다시 시도해주세요",
+          error
+        );
+      });
   };
 
-  const form = `flex flex-col items-center justify-center w-2/5 pt-8 pb-6`
-  const ipt = `w-full py-2 px-4 mb-7 border-b-2 border-color-3 outline-none`
-  const btn = `w-full bg-color-3 py-2 text-xl rounded-lg`
+  const form = `flex flex-col items-center justify-center w-2/5 pt-8 pb-6`;
+  const ipt = `w-full py-2 px-4 mb-7 border-b-2 border-color-3 outline-none`;
+  const btn = `w-full bg-color-3 py-2 text-xl rounded-lg`;
 
-  return(
+  return (
     <form className={form} onSubmit={handleLogin}>
-      <input 
+      <input
         className={ipt}
-        type="text" 
-        value={username} 
+        type="text"
+        value={username}
         placeholder="아이디 입력"
-        onChange={e => setUsername(e.target.value)}  
+        onChange={(e) => setUsername(e.target.value)}
         autoComplete="username"
         required
       />
-      <input 
+      <input
         className={ipt}
-        type="password" 
+        type="password"
         value={password}
         placeholder="비밀번호 입력"
-        onChange={e => setPassword(e.target.value)}
+        onChange={(e) => setPassword(e.target.value)}
         autoComplete="current-password"
         required
       />
-      <button className={btn} type="submit">로그인</button>
+      <button className={btn} type="submit">
+        로그인
+      </button>
     </form>
   );
 };
--- book-forest/src/store/store.js
+++ book-forest/src/store/store.js
@@ -1,8 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage/session";
 import rootReducer from "./rootReducer";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user", "page"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
+  devTools: import.meta.env.DEV,
 });
 
+export const persistor = persistStore(store);
 export default store;
