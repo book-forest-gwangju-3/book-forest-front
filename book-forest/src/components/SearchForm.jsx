a36bb753ed8d039cc766bb4a37337e49a0115ec2
junhee <limjunhee4575@gmail.com>
2024-06-28 12:42:14 +0900

    feat.독후감 검색 기능 완성

--- book-forest/src/components/SearchForm.jsx
+++ book-forest/src/components/SearchForm.jsx
@@ -1,6 +1,6 @@
 import { SlMagnifier } from "react-icons/sl";
 // 호출할때 props로 type={"max-w-md"} 넣으면 짧은 검색창, 안넣으면 긴 검색창
-const SearchForm = ({ text, type }) => {
+const SearchForm = ({ text, type, value, onChange }) => {
   const containerClass = `relative mx-auto text-gray-600 w-full ${type}`;
   const inputClass =
     "border border-gray-300 bg-white h-10 px-5 pr-10 rounded-lg text-sm focus:outline-none w-full";
@@ -8,7 +8,13 @@ const SearchForm = ({ text, type }) => {
   const iconClass = "text-gray-600 h-4 w-4 fill-current";
   return (
     <div className={containerClass}>
-      <input placeholder={text} type="search" className={inputClass} />
+      <input
+        placeholder={text}
+        type="search"
+        className={inputClass}
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit" className={buttonClass}>
         <SlMagnifier className={iconClass} />
       </button>
