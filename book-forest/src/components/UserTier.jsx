35f603051c6579b42a80a0a9f489682f2bc93adb
윤하연 <yhy5049@naver.com>
2024-06-30 23:39:09 +0900

    feat.랭킹 페이지 구현 완료

--- book-forest/src/components/UserTier.jsx
+++ book-forest/src/components/UserTier.jsx
@@ -1,38 +1,62 @@
-import { FaTrophy } from "react-icons/fa";
+import { useSelector } from "react-redux"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const UserTier = () => {
-  // axios로 로그인한 유저 데이터 받아오기
+  // 로그인한 유저 데이터 받아오기
+  const [userRanking, setUserRanking] = useState({});
+  const token = useSelector((state) => state.user.token);
+  const isLogin = useSelector((state) => state.user.isLogin);
 
-  
-  // 임시 데이터
-  const data = {
-    nickname: 'NickName',
-    rank: 32,
-    tier: 'BRONZE',
-    percent: 25,
-    point: 1020,
-  }
+  useEffect(() => {
+    if (!token) return;
 
-  const container = `w-full flex justify-between items-end border-b-2 border-color-16 mt-10 mb-8 px-8`
-  const left = `flex text-3xl`
-  const right = `flex text-color-17`
+    const fetchUserTier = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/user/my-tier`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = response.data;
+        // 100에서 백분율을 뺀 값으로 상위 퍼센티지 계산
+        data.upperPercent = 100 - data.position;
+        setUserRanking(data);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching user tier:', error);
+      }
+    };
+
+    fetchUserTier();
+  }, [token]);
+
+  const container = `w-full flex justify-between items-end border-b-2 border-color-16 mt-10 mb-8 px-8`;
+  const left = `flex text-3xl`;
+  const right = `flex text-color-17`;
 
   return (
-    <div className={container}>
-      <div className={left}>
-        <div className="mr-10">
-          <span className="text-color-9">{data.rank}</span>
-          <span className="text-base text-color-17 ml-1">위</span>
+    <>
+      {isLogin ? (
+        <div className={container}>
+          <div className={left}>
+            <div className="mr-10">
+              <span className="text-color-9">?</span>
+              <span className="text-base text-color-17 ml-1">위</span>
+            </div>
+            <div>{userRanking.user?.nickname}</div>
+          </div>
+          <div className={right}>
+            <div className="mr-8 text-color-4 font-bold">{userRanking.tierName}</div>
+            <div className="mr-8">상위 {userRanking.upperPercent} %</div>
+            <div>{userRanking.exp} p</div>
+          </div>
         </div>
-        <div>{data.nickname}</div>
-      </div>
-      <div className={right}>
-        <div className="mr-8 text-color-4 font-bold">{data.tier}</div>
-        <div className="mr-8">상위 {data.percent} %</div>
-        <div>{data.point} p</div>
-      </div>
-    </div>
-  )
-}
+      ) :
+      <div className="mt-10"></div>
+      }
+    </>
+  );
+};
 
-export default UserTier
+export default UserTier;
