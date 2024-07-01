e152cd4264c195ff91c2071dc40868a1c91215a4
junhee <limjunhee4575@gmail.com>
2024-07-01 15:47:51 +0900

    feat.주간 인기 독후감 조회 기능 완성

--- book-forest/src/components/ReportCard.jsx
+++ book-forest/src/components/ReportCard.jsx
@@ -1,37 +1,37 @@
 import Button from "./Button";
-const ReportCard = ({ img, text }) => {
-  // 아예 pages/Report에서 데이터를 받아서 여기로 줘야됨.
-  // props : id, img, title, writer, content(길이 제한)
+import { useNavigate } from "react-router-dom";
+const ReportCard = ({ item, text }) => {
+  const nav = useNavigate();
+
   const containerClass = "flex items-center justify-center my-6";
   const wrapperClass =
     "relative flex w-full flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md";
-  const textWrapperClass = "p-6 pl-10";
+  const textWrapperClass = "p-6 pl-10 w-3/5";
   const rankClass = "mb-4 block font-semibold text-pink-500 antialiased";
   const titleClass =
-    "mb-2 block text-2xl font-semibold text-blue-gray-900 antialiased";
+    "mb-2 block text-2xl font-semibold text-blue-gray-900 antialiased overflow-hidden";
   const writerClass = "mb-2 block";
   const contentClass =
-    "mb-8 block font-normal leading-relaxed text-gray-700 antialiased";
+    "mb-8 block font-normal leading-relaxed text-gray-700 antialiased w-full";
   const imgWrapperClass =
-    "p-6 pl-44 relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none";
-  const imgClass = "w-auto h-auto object-cover";
+    "p-6 flex items-center justify-center w-2/5 overflow-hidden rounded-xl rounded-r-none";
+  const imgClass = "w-36 h-56 object-cover";
   return (
     <div className={containerClass}>
       <div className={wrapperClass}>
         <div className={textWrapperClass}>
           <h6 className={rankClass}>{text}</h6>
-          <h4 className={titleClass}>도취된 권력, 타락한 정의를 읽고</h4>
-          <p className={writerClass}>hayeon</p>
-          <p className={contentClass}>
-            Like so many organizations these days, Autodesk is a company in
-            transition. It was until recently a traditional boxed software
-            company selling licenses. Yet its own business model disruption is
-            only part of the story
-          </p>
-          <Button text={"더보기"} color={"bg-pink-500 text-white"} />
+          <h4 className={titleClass}>{item.title}</h4>
+          <p className={writerClass}>{item.user.username}</p>
+          <p className={contentClass}>{item.content}</p>
+          <Button
+            onClick={() => nav(`/report/${item.id}`)}
+            text={"더보기"}
+            color={"bg-pink-500 text-white"}
+          />
         </div>
         <div className={imgWrapperClass}>
-          <img src={img} alt="image" className={imgClass} />
+          <img src={item.book.cover} alt="image" className={imgClass} />
         </div>
       </div>
     </div>
